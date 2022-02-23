bce2a5b17cf24e1f2874eee934fd992c1c75c59d
Igor <igorlyakhoff@gmail.com>
2022-02-23 19:53:36 +0300

    Added bad tests

--- src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,32 @@
 package com.epam.ld.module2.testing;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MailServerTest {
+
+    @Test
+    public void sendMessageTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void createMessengerTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void updateAndSendMessageTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void replacesVariableTest(){
+        fail("Not implementation");
+    }
+
+
+
 }
--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,43 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 public class TemplateEngineTest {
+    @Test
+    public void generateTemplateTest(){
+        fail();
+    }
+
+    @Test
+    public void getClientAddressTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void setClientAddressTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void generateMessageTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void withoutOneValuesTemplateTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void extraValuesInTemplateTest(){
+        fail("Not implementation");
+    }
+
+    @Test
+    public void supportBracketValuesTest(){
+        fail("Not implementation");
+    }
 }
